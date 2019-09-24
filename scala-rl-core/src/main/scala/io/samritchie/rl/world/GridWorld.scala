2a05ad743f0d4be13b416c612e1378e4d00816d3
Sam Ritchie <sritchie09@gmail.com>
2019-09-24 09:29:08 -0600

    add id

--- scala-rl-core/src/main/scala/io/samritchie/rl/world/GridWorld.scala
+++ scala-rl-core/src/main/scala/io/samritchie/rl/world/GridWorld.scala
@@ -104,36 +104,4 @@ case class GridWorld(
       case Some((newPosition, reward)) =>
         (reward, copy(grid = grid.teleportUnsafe(newPosition)))
     }
-
-  /**
-    * Draws a plot.
-    *
-    *
-    def draw_image(image):
-    fig, ax = plt.subplots()
-    ax.set_axis_off()
-    tb = Table(ax, bbox=[0, 0, 1, 1])
-
-    nrows, ncols = image.shape
-    width, height = 1.0 / ncols, 1.0 / nrows
-
-    # Add cells
-    for (i, j), val in np.ndenumerate(image):
-        tb.add_cell(i, j, width, height, text=val,
-                    loc='center', facecolor='white')
-
-    # Row and column labels...
-    for i in range(len(image)):
-        tb.add_cell(i, -1, width, height, text=i+1, loc='right',
-                    edgecolor='none', facecolor='none')
-        tb.add_cell(-1, i, width, height/2, text=i+1, loc='center',
-                    edgecolor='none', facecolor='none')
-
-    ax.add_table(tb)
-    */
-  def render(): Unit = {
-    println("For now, we'll just print a string...")
-    println("Rendered!")
-  }
-
 }
