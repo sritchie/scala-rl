f06ce967dddb93b30ad670f69f187ade3519e48d
Sam Ritchie <sritchie09@gmail.com>
2019-09-29 11:45:03 -0600

    add tests

--- scala-rl-book/src/main/scala/io/samritchie/rl/book/Chapter3.scala
+++ scala-rl-book/src/main/scala/io/samritchie/rl/book/Chapter3.scala
@@ -27,19 +27,25 @@ object Chapter3 {
   val gamma: Double = 0.9
   val emptyFn = ValueFunction.decaying[Position](gamma)
 
+  def notConverging(iterations: Long): Boolean = iterations >= allowedIterations
+
   /**
     Note... this version, following the python code, checks that the sum of all
     differences is less than epsilon. In the next chapter we use the max
     function instead here to get this working, to check that the maximum delta
     is less than epsilon.
     */
-  def hasConverged[Obs](
+  def valueFunctionConverged[Obs](
+      l: ValueFunction[Obs],
+      r: ValueFunction[Obs]
+  ): Boolean = ValueFunction.diff(l, r, epsilon)(_ + _)
+
+  def shouldStop[Obs](
       l: ValueFunction[Obs],
       r: ValueFunction[Obs],
       iterations: Long
   ): Boolean =
-    (iterations >= allowedIterations) ||
-      ValueFunction.diff(l, r, epsilon)(_ + _)
+    notConverging(iterations) || valueFunctionConverged(l, r)
 
   def toTable(conf: GridWorld.Config, f: Position => Real): Iterable[Iterable[Real]] =
     Grid
@@ -50,51 +56,44 @@ object Chapter3 {
       .toSeq
       .map(_.toSeq)
 
+  def printFigure(pair: (ValueFunction[Position], Long), title: String): Unit = {
+    val (valueFn, iterations) = pair
+    println(s"${title}:")
+    println(Tabulator.format(toTable(gridConf, valueFn.stateValue(_).get)))
+    println(s"That took $iterations iterations, for the record.")
+  }
+
   /**
     * This is Figure 3.2, with proper stopping conditions and
     * everything. Lots of work to go.
     *   */
-  def figureThreeTwo(): Unit = {
-    val (valueFn, iterations) =
-      ValueFunction.sweepUntil[Move, Position, Double](
-        Random.id[Move, Double],
-        emptyFn,
-        gridConf.stateSweep,
-        hasConverged _,
-        inPlace = true
-      )
-
-    // Display stats for threeTwo...
-    println("Figure 3.2:")
-    println(Tabulator.format(toTable(gridConf, valueFn.stateValue(_).get)))
-    println(s"That took $iterations iterations, for the record.")
-  }
+  def threeTwo: (ValueFunction[Position], Long) =
+    ValueFunction.sweepUntil[Move, Position, Double](
+      Random.id[Move, Double],
+      emptyFn,
+      gridConf.stateSweep,
+      shouldStop _,
+      inPlace = true
+    )
 
   /**
     * This is Figure 3.5. This is currently working!
     */
-  def figureThreeFive(): Unit = {
-    val (valueFn, iterations) =
-      ValueFunction.sweepUntil[Move, Position, Double](
-        Greedy[Move, Position, Double](emptyFn),
-        emptyFn,
-        gridConf.stateSweep,
-        hasConverged _,
-        inPlace = true
-      )
-
-    // Display stats for threeFive...
-    println("Figure 3.5:")
-    println(Tabulator.format(toTable(gridConf, valueFn.stateValue(_).get)))
-    println(s"That took $iterations iterations, for the record.")
-  }
+  def threeFive: (ValueFunction[Position], Long) =
+    ValueFunction.sweepUntil[Move, Position, Double](
+      Greedy[Move, Position, Double](emptyFn),
+      emptyFn,
+      gridConf.stateSweep,
+      shouldStop _,
+      inPlace = true
+    )
 
   /**
     * This currently works, and displays rough tables for each of the required
     * bits.
     */
   def main(items: Array[String]): Unit = {
-    figureThreeTwo()
-    figureThreeFive()
+    printFigure(threeTwo, "Figure 3.2")
+    printFigure(threeFive, "Figure 3.5")
   }
 }
---
+++ scala-rl-book/src/test/scala/io/samritchie/rl/book/Chapter3Spec.scala
@@ -0,0 +1,89 @@
+package io.samritchie.rl
+package book
+
+import com.stripe.rainier.compute.Real
+import io.samritchie.rl.util.Grid
+import io.samritchie.rl.value.{Decaying, MapValueFunction}
+import org.scalatest.FunSuite
+
+/**
+  * And this is a placeholder for basic tests.
+  */
+class Chapter3Spec extends FunSuite {
+  import Grid.Position
+
+  val gamma = 0.9
+  val epsilon = 1e-4
+
+  test("Figure 3.2's value function matches the gold set") {
+    val (actual, _) = Chapter3.threeTwo
+    val expected = MapValueFunction(
+      Map(
+        Position.of(0, 0) -> Real(3.3090),
+        Position.of(0, 1) -> Real(8.7893),
+        Position.of(0, 2) -> Real(4.4276),
+        Position.of(0, 3) -> Real(5.3223),
+        Position.of(0, 4) -> Real(1.4921),
+        Position.of(1, 0) -> Real(1.5216),
+        Position.of(1, 1) -> Real(2.9923),
+        Position.of(1, 2) -> Real(2.2501),
+        Position.of(1, 3) -> Real(1.9075),
+        Position.of(1, 4) -> Real(0.5474),
+        Position.of(2, 0) -> Real(0.0508),
+        Position.of(2, 1) -> Real(0.7381),
+        Position.of(2, 2) -> Real(0.6731),
+        Position.of(2, 3) -> Real(0.3582),
+        Position.of(2, 4) -> Real(-0.4031),
+        Position.of(3, 0) -> Real(-0.9735),
+        Position.of(3, 1) -> Real(-0.4354),
+        Position.of(3, 2) -> Real(-0.3548),
+        Position.of(3, 3) -> Real(-0.5855),
+        Position.of(3, 4) -> Real(-1.1830),
+        Position.of(4, 0) -> Real(-1.8576),
+        Position.of(4, 1) -> Real(-1.3452),
+        Position.of(4, 2) -> Real(-1.2292),
+        Position.of(4, 3) -> Real(-1.4229),
+        Position.of(4, 4) -> Real(-1.9751)
+      ).mapValues(Decaying(_, gamma)),
+      Decaying(0.0, gamma)
+    )
+
+    assert(ValueFunction.diff(actual, expected, epsilon)(_.max(_)))
+  }
+
+  test("Figure 3.5's value function matches the gold set") {
+    val (actual, _) = Chapter3.threeFive
+    val expected = MapValueFunction(
+      Map(
+        Position.of(0, 0) -> Real(19.8896),
+        Position.of(0, 1) -> Real(24.4194),
+        Position.of(0, 2) -> Real(21.9774),
+        Position.of(0, 3) -> Real(19.4194),
+        Position.of(0, 4) -> Real(17.4774),
+        Position.of(1, 0) -> Real(19.7797),
+        Position.of(1, 1) -> Real(21.9774),
+        Position.of(1, 2) -> Real(19.7797),
+        Position.of(1, 3) -> Real(17.8017),
+        Position.of(1, 4) -> Real(16.0215),
+        Position.of(2, 0) -> Real(17.8017),
+        Position.of(2, 1) -> Real(19.7797),
+        Position.of(2, 2) -> Real(17.8017),
+        Position.of(2, 3) -> Real(16.0215),
+        Position.of(2, 4) -> Real(14.4194),
+        Position.of(3, 0) -> Real(16.0215),
+        Position.of(3, 1) -> Real(17.8017),
+        Position.of(3, 2) -> Real(16.0215),
+        Position.of(3, 3) -> Real(14.4194),
+        Position.of(3, 4) -> Real(12.9774),
+        Position.of(4, 0) -> Real(14.4194),
+        Position.of(4, 1) -> Real(16.0215),
+        Position.of(4, 2) -> Real(14.4194),
+        Position.of(4, 3) -> Real(12.9774),
+        Position.of(4, 4) -> Real(11.6797)
+      ).mapValues(Decaying(_, gamma)),
+      Decaying(0.0, gamma)
+    )
+
+    assert(ValueFunction.diff(actual, expected, epsilon)(_.max(_)))
+  }
+}
