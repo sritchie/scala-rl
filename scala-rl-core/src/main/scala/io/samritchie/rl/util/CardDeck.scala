d069bd76946cf25ecc99b034a1422859c1e5eda0
Sam Ritchie <sritchie09@gmail.com>
2019-10-08 10:56:17 -0600

    get game working

--- scala-rl-core/src/main/scala/io/samritchie/rl/util/CardDeck.scala
+++ scala-rl-core/src/main/scala/io/samritchie/rl/util/CardDeck.scala
@@ -8,34 +8,48 @@ import com.stripe.rainier.core.Generator
 
 object CardDeck {
   sealed trait Rank extends Any with Product with Serializable
-  final case object Jack extends Rank
-  final case object Queen extends Rank
-  final case object King extends Rank
-  final case object Ace extends Rank
-  final case class Number(value: Int) extends AnyVal with Rank
+  object Rank {
+    val all: Vector[Rank] = Vector(Jack, King, Queen, Ace) ++ (2 to 10).map(Number(_))
+    final case object Jack extends Rank
+    final case object Queen extends Rank
+    final case object King extends Rank
+    final case object Ace extends Rank
+    final case class Number(value: Int) extends AnyVal with Rank
+
+    implicit val rankOrd: Ordering[Rank] =
+      Ordering.by(all.indexOf(_))
+  }
 
   sealed trait Suit extends Product with Serializable
-  final case object Spades extends Suit
-  final case object Hearts extends Suit
-  final case object Clubs extends Suit
-  final case object Diamonds extends Suit
+  object Suit {
+    val all: Vector[Suit] = Vector(Spades, Hearts, Clubs, Diamonds)
+    final case object Spades extends Suit
+    final case object Hearts extends Suit
+    final case object Clubs extends Suit
+    final case object Diamonds extends Suit
+
+    implicit val suitOrd: Ordering[Suit] =
+      Ordering.by(all.indexOf(_))
+  }
 
   case class Card(suit: Suit, rank: Rank)
+  object Card {
+    val all: Vector[Card] = for {
+      suit <- Suit.all
+      rank <- Rank.all
+    } yield Card(suit, rank)
 
-  val suits: Vector[Suit] = Vector(Spades, Hearts, Clubs, Diamonds)
-  val ranks: Vector[Rank] = Vector(Jack, King, Queen, Ace) ++ (2 to 10).map(Number(_))
-  val sortedDeck: Vector[Card] = for {
-    suit <- suits
-    rank <- ranks
-  } yield Card(suit, rank)
+    val deck: Set[Card] = all.to[Set]
 
-  val deck: Set[Card] = sortedDeck.to[Set]
+    implicit val ordering: Ordering[Card] =
+      Ordering.by(card => (card.suit, card.rank))
+  }
 
   /**
     Generates cards from an infinite stream, with replacement.
     */
   val basic: Generator[Card] = {
-    val size = deck.size
-    Generator.from((r, _) => sortedDeck(r.int(size)))
+    val size = Card.deck.size
+    Generator.from((r, _) => Card.all(r.int(size)))
   }
 }
--- scala-rl-core/src/main/scala/io/samritchie/rl/world/Blackjack.scala
+++ scala-rl-core/src/main/scala/io/samritchie/rl/world/Blackjack.scala
@@ -4,74 +4,184 @@
 package io.samritchie.rl
 package world
 
-import com.stripe.rainier.core.{Categorical, Generator}
+import cats.{Id, Monad}
+import com.stripe.rainier.cats._
+import com.stripe.rainier.core.Generator
+import io.samritchie.rl.util.CardDeck
 
 object Blackjack {
-  case class Config() {
-    def build(startingState: Observation): Blackjack =
-      Blackjack(this, startingState)
+  import CardDeck.{Card, Rank}
 
-    def allStates: Generator[Observation] =
-      Cat
-        .seq(
-          for {
-            currentSum <- 12 to 21
-            aceCount <- 0 to 1
-            dealerSum <- 1 to 10
-          } yield Observation(Sum(currentSum), Sum(aceCount), Card(dealerSum))
-        )
-        .toRainier
-        .generator
-
-    // Get this into a better generator state.
-    def stateGen: Generator[Blackjack] =
-      allStates.map(Blackjack(this, _))
-  }
-
+  // TODO - to make this more legit we need to add the ability to bet.
   sealed trait Action extends Product with Serializable
   object Action {
     final case object Hit extends Action
     final case object Stay extends Action
   }
-  case class Sum(n: Int) extends AnyVal
-  case class Card(value: Int) extends AnyVal {
-    def isAce: Boolean = value == 1
+
+  def cardValue(card: Card): Int = card.rank match {
+    case Rank.Ace                           => 11
+    case Rank.Jack | Rank.Queen | Rank.King => 10
+    case Rank.Number(n)                     => n
   }
-  case class Observation(
-      currentSum: Sum,
-      aceCount: Sum,
-      dealerCard: Card
+
+  /**
+    TODO - to make this solid, the Hand should actually maintain a sorted state,
+    so that we can use it as the key in a hashmap. It's fine for now, since this
+    state is actually going to get dropped down into a state viewable by a
+    policy.
+    */
+  case class Hand(showing: Seq[Card], hidden: Seq[Card]) {
+    def takeCard(card: Card, isShowing: Boolean): Hand =
+      if (isShowing)
+        copy(showing = showing :+ card)
+      else
+        copy(hidden = hidden :+ card)
+
+    val usableAce: Boolean = Hand.aceCount(cards) > 0
+    val totalScore: Int = Hand.score(cards)
+    val showingScore: Int = Hand.score(showing)
+
+    def cards: Seq[Card] = showing ++ hidden
+    def busted: Boolean = totalScore > 21
+    def showAll: Hand = if (hidden.isEmpty) this else Hand(cards, Seq.empty)
+  }
+
+  object Hand {
+    val empty = Hand(Seq.empty, Seq.empty)
+
+    def aceCount(cards: Seq[Card]): Int = cards.filter(_.rank == Rank.Ace).size
+    def maxPoints(cards: Seq[Card]): Int = cards.foldLeft(0)((acc, c) => acc + cardValue(c))
+
+    def score(cards: Seq[Card]): Int = {
+      def loop(points: Int, acesLeft: Int): Int =
+        if (acesLeft <= 0)
+          points
+        else if (points > 21)
+          loop(points - 10, acesLeft - 1)
+        else points
+
+      loop(maxPoints(cards), aceCount(cards))
+    }
+  }
+
+  /**
+    This is the actual, full rich game.
+    */
+  case class Game(player: Hand, dealer: Hand) {
+    def agentView: AgentView = AgentView(
+      player.usableAce,
+      player.totalScore,
+      dealer.showingScore
+    )
+    def showAll: Game = Game(player.showAll, dealer.showAll)
+  }
+  object Game {
+    val empty = Game(Hand.empty, Hand.empty)
+  }
+
+  def dealerHand(getCard: Generator[Card]): Generator[Hand] =
+    for {
+      showing <- getCard
+      hidden <- getCard
+    } yield Hand(Seq(showing), Seq(hidden))
+
+  def playerHand(getCard: Generator[Card]): Generator[Hand] =
+    Monad[Generator].tailRecM[Hand, Hand](Hand.empty) { hand =>
+      if (hand.totalScore < 12)
+        getCard.map(card => Left(hand.takeCard(card, true)))
+      else
+        Generator.constant(Right(hand))
+    }
+
+  def gameGenerator(getCard: Generator[Card]): Generator[Game] =
+    for {
+      player <- playerHand(getCard)
+      dealer <- dealerHand(getCard)
+    } yield Game(player, dealer)
+
+  case class Config(getCard: Generator[Card]) {
+    def build(startingState: Game): Blackjack =
+      Alive(this, startingState)
+
+    def stateGen: Generator[Blackjack] =
+      gameGenerator(getCard).map(build(_))
+  }
+
+  /**
+    This is what the agent is allowed to see.
+    */
+  case class AgentView(
+      usableAce: Boolean,
+      playerSum: Int,
+      dealerSum: Int
   )
 
-  val nextCard: Generator[Card] =
-    Categorical.list((1 to 14).map(n => Card(math.min(10, n)))).generator
+  def dealerPolicy[S[_]](hitBelow: Int): Policy[Action, Game, Double, Id, S] =
+    new Policy[Action, Game, Double, Id, S] {
+      override def choose(state: State[Action, Game, Double, S]): Action = {
+        val hand = state.observation.dealer
+        if (hand.totalScore < hitBelow) Action.Hit else Action.Stay
+      }
+    }
+}
+
+sealed trait Blackjack extends State[Blackjack.Action, Blackjack.Game, Double, Generator]
+
+case class Dead(game: Blackjack.Game) extends Blackjack {
+  import Blackjack.{Action, Game}
+
+  override val observation = game.showAll
+  override def dynamics[O2 >: Game]: Map[Action, Generator[(Double, Blackjack)]] =
+    Map.empty
 }
 
 /**
   So this is PROBABLY a place where I actually need the full state, so I can
   track that the dealer has two cards, generated randomly.
   */
-case class Blackjack(
-    config: Blackjack.Config,
-    observation: Blackjack.Observation
-) extends State[Blackjack.Action, Blackjack.Observation, Double, Generator] {
-  import Blackjack.{Action, Card, Observation}
+case class Alive(config: Blackjack.Config, game: Blackjack.Game) extends Blackjack {
+  import Blackjack.{Action, Game, Hand}
+  import CardDeck.Card
 
-  def dealerAce: Boolean = observation.dealerCard.isAce
+  override val observation = game
 
   // I think we're not going to be able to ever need to call this from the
   // current set of techniques... so maybe we move this to some place where we
   // have an expected value?
-  def dynamics[O2 >: Observation]: Map[Action, Generator[(Double, State[Action, O2, Double, Generator])]] =
-    if (someoneWon)
-      Map.empty
-    else
-      Map(
-        Action.Hit -> Blackjack.nextCard.map(hit(_)),
-        Action.Stay -> Blackjack.nextCard.map(dealerTurn(_))
-      )
+  override def dynamics[O2 >: Game]: Map[Action, Generator[(Double, Blackjack)]] =
+    Map(
+      Action.Hit -> config.getCard.map(hit(_)),
+      Action.Stay -> dealerTurn(config.getCard)
+    )
 
-  def someoneWon: Boolean = ???
-  def hit(card: Card): (Double, Blackjack) = ???
-  def dealerTurn(card: Card): (Double, Blackjack) = ???
+  private def hit(card: Card): (Double, Blackjack) = {
+    val newGame = Game(game.player.takeCard(card, true), game.dealer)
+    if (newGame.player.busted)
+      (-1, Dead(newGame))
+    else
+      (0, copy(game = newGame))
+  }
+
+  private def endingReward(player: Hand, dealer: Hand): Int =
+    if (dealer.busted || player.totalScore > dealer.totalScore)
+      1
+    else if (game.player.totalScore == dealer.totalScore)
+      0
+    else
+      -1
+
+  // This is really a policy interaction... we should allow these to ping back
+  // and forth. Can I just PLAY a policy?
+  // TODO convert this to use the dealer policy that's in the config.
+  private def dealerTurn(getCard: Generator[Card]): Generator[(Double, Blackjack)] =
+    Monad[Generator].tailRecM[Hand, (Double, Blackjack)](game.dealer) { hand =>
+      if (hand.totalScore < 17)
+        getCard.map(card => Left(hand.takeCard(card, true)))
+      else {
+        val reward = endingReward(game.player, hand)
+        val dead = Dead(game.copy(dealer = hand))
+        Generator.constant(Right((reward, dead)))
+      }
+    }
 }
